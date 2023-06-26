cca5cb9ebe59ff8df5bf05142d2904b75e66ca20
LudvigOrest <ludvig.orest@live.se>
2023-06-26 12:15:08 +0200

    Fortsatte på funktionaliteten på hemsidan i JS

--- main.js
+++ main.js
@@ -1,49 +1,16 @@
-fetchAPI();
-fetchSingleTodo(2);
-addTodo("hello man");
-editTodo("goodbye man", 3).then(
-    fetchSingleTodo(3)
-);
-
-fetchSingleTodo(3);
-
-function fetchAPI() {
-    fetch('https://dummyjson.com/todos')
-    .then (res => res.json())
-    .then (console.log);
+function Todo(todoName, todoDescription, todoCreated, todoIsFinished, todoFinished) {
+    this.todoName = todoName;
+    this.todoDescription = todoDescription;
+    this.todoCreated = todoCreated;
+    this.todoIsFinished = todoIsFinished;
+    this.todoFinished = todoFinished;
 }
 
-function fetchSingleTodo(number) {
-    fetch('https://dummyjson.com/todos/' + number)
-    .then (res => res.json())
-    .then (console.log);
-}
+let todo1 = new Todo("Skrapa rutan", "skrapa is från framrutan", "2023-06-26", false);
 
-async function addTodo(todoText) {
-    fetch('https://dummyjson.com/todos/add', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            todo: todoText.toString(),
-            completed: false,
-            userId: 5,
-        })
-    })
-    .then(res => res.json())
-    .then(console.log);
-}
+let arrayOfTodo = [];
+let arrayOfFinished = [];
 
-async function editTodo(todoText, postId) {
-    fetch('https://dummyjson.com/todos/' + postId, {
-        method: 'PUT', /* or PATCH */
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({
-            todo: todoText.toString(),
-            completed: false,
-        })
-    })
-    .then(res => res.json())
-    
-}
+arrayOfTodo.push(todo1);
 
-//Ändringarna eller nya todo:s sparas inte på API:et och kan därför inte hämtas senare. 
+console.log(arrayOfTodo);
